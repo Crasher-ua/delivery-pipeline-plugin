57f5d3c5872ad3e4c909de6214cfd4e424adeff3
Yaroslav Kazmirchuk <Yaroslav_Kazmirchuk@epam.com>
2017-09-19 16:12:29 +0300

    Moved repeating JS logic to reusable script (build stage header)

--- src/main/webapp/themes/overview/pipe.js
+++ src/main/webapp/themes/overview/pipe.js
@@ -128,15 +128,8 @@ function pipelineUtils() {
 
                         html.push('<div class="pipeline-cell">');
                         html.push('<div id="' + getStageId(stage.id + '', i) + '" class="stage ' + getStageClassName(stage.name) + '">');
-                        html.push('<div class="stage-header"><div class="stage-name">' + htmlEncode(stage.name) + '</div>');
 
-                        if (!pipeline.aggregated) {
-                            html.push('<div class="clear"></div></div>');
-                        } else {
-                            var stageversion = stage.version || 'N/A';
-                            html.push(' <div class="stage-version">' + htmlEncode(stageversion) + '</div><div class="clear"></div></div>');
-                        }
-
+                        buildStageHeader(html, stage, pipeline, true);
                         buildAllStageTasks(stage, i, tasks, data, html, view, pipeline, component, lastUpdate);
 
                         if (pipeline.aggregated && stage.changes && stage.changes.length > 0) {
